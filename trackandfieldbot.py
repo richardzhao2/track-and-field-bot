39aecffe051f7048e25efc28fc2ca3b8b0a6abdd
Zhao <rzhao@astm.org>
2018-07-18 16:28:54 -0400

    adding some print statemenets

--- trackandfieldbot.py
+++ trackandfieldbot.py
@@ -21,10 +21,30 @@ raceStarted = False
 
 print("starting bot...")
 time.sleep(3)
-while(running==True and counter < 400):
+while(running==True and counter < 50):
+
+
 	pyautogui.dragTo(670, 862, .1, button='left')
 	pyautogui.dragTo(800, 812,.1,button='left')
 	counter+=1
 
 print("bot closing...")
 sys.exit()
+
+
+def main():
+    print("Track and Field 100m Bot by Richard Zhao")
+    print("Programmed specifically for 1920x1080p resolution")
+    
+    #SELECT BOT MODE
+    option = 1
+
+    print("Farming (", option, ") Mode...")
+    if option==0:
+        farmSimple(999999)
+    else:
+        farmMove()
+    print("Bot Stopped.")
+
+if __name__ == '__main__':
+    main()
