2cb39c1c951a1812546f6870e4aa2cd0b112d55f
richardzhao2 <rzhao2@andrew.cmu.edu>
2018-07-17 22:28:26 -0400

    testing clicks

--- trackandfieldbot.py
+++ trackandfieldbot.py
@@ -9,5 +9,11 @@ simple clicker bot to play Facebook messenger's 100m track and field
 img1 = PIL.ImageGrab.grab(bbox=(0,0,100,100))
 print(img1)
 
+counter = 0
+pyautogui.PAUSE = 0.05
 
-pyautogui.moveTo(0, 50)
+while(counter < 50):
+	pyautogui.click(x=100, y=200)
+	pyautogui.click(x=150, y=200)
+	counter += 1
+	print(counter)
