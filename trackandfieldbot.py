c7f24f9efaf107a887c79e587083d38c39a9594d
richardzhao2 <rzhao2@andrew.cmu.edu>
2018-07-18 00:03:02 -0400

    working bot fastest = 7.8 need to work on automating with opencv or audio processing

--- trackandfieldbot.py
+++ trackandfieldbot.py
@@ -1,19 +1,30 @@
 import numpy
 import pyautogui
 import PIL
+import time
+import sys
 
 """
 simple clicker bot to play Facebook messenger's 100m track and field
 """
 
 img1 = PIL.ImageGrab.grab(bbox=(0,0,100,100))
-print(img1)
+
+BUTTON1_COORDINATE = (618, 812, 104, 100)
+BUTTON1_CENTER = (670,862)
+BUTTON2_LOCATION = (800,812)
 
 counter = 0
-pyautogui.PAUSE = 0.05
+pyautogui.PAUSE = 0.01
+running = True
+raceStarted = False
 
-while(counter < 50):
-	pyautogui.click(x=100, y=200)
-	pyautogui.click(x=150, y=200)
-	counter += 1
-	print(counter)
+print("starting bot...")
+time.sleep(3)
+while(running==True and counter < 400):
+	pyautogui.dragTo(670, 862, .1, button='left')
+	pyautogui.dragTo(800, 812,.1,button='left')
+	counter+=1
+
+print("bot closing...")
+sys.exit()
